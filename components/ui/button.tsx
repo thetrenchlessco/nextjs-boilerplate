f71f6c36b987105f4f07617faa39a46484b09b53
thetrenchlessco <10xaustin@gmail.com>
2026-02-07 00:18:17 -0700

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,1 +1,10 @@
-
+export function Button({ children, className = '', ...props }: any) {
+  return (
+    <button
+      {...props}
+      className={`px-4 py-2 rounded-xl font-medium ${className}`}
+    >
+      {children}
+    </button>
+  )
+}
