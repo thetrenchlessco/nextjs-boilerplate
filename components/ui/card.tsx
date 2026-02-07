3f66d7707e365a98a4f30277d0fa9fcae07f5408
thetrenchlessco <10xaustin@gmail.com>
2026-02-07 00:20:00 -0700

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,1 +1,7 @@
+export function Card({ children, className = '' }: any) {
+  return <div className={className}>{children}</div>
+}
 
+export function CardContent({ children, className = '' }: any) {
+  return <div className={className}>{children}</div>
+}
